f3477854c5935cdd8774c8cd35c388e8001061bc
altinbarin <furkanaltinbarin@gmail.com>
2023-11-28 20:48:57 +0300

    28 Kasım JWT

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -33,6 +33,8 @@ namespace Business.Concrete
         }
 
 
+        //Claim
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
